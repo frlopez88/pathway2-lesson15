f4b72b6de6626a0e4c45c42d56bcd2d31f06c15b
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-10-02 17:55:40 -0600

    Commit Append Child

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ for ( let i = 0; i < arrayInputs.length; i++ ){
     arrayInputs[i].style.display = "block"
     arrayInputs[i].style.marginBottom ="20px"
 }
+
+let tr = document.createElement("tr")
+
+let th1 = document.createElement("th")
+let th2 = document.createElement("th")
+let th3 = document.createElement("th")
+
+th1.textContent = "Name"
+th2.textContent = "Age"
+th3.textContent = "Gender"
+
+tr.appendChild(th1)
+tr.appendChild(th2)
+tr.appendChild(th3)
+
+let table1 = document.getElementById("table1")
+
+table1.appendChild(tr)
